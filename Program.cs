696af9911174c07293672d75cd184bddb5835feb
Morten Middelboe Pedersen <MPD@simcorp.com>
2017-09-01 10:03:23 +0200

    3 commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
